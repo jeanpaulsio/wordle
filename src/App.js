4c3351055489208a3156eb5b4cb5cbed4ab5ee36
JP Sio <hello@jpsio.me>
2022-02-12 15:29:13 -0800

    Use backspace svg

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const QWERTY = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
 const ASDF = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
 const ZXCV = ["z", "x", "c", "v", "b", "n", "m"];
 const ENTER = "enter";
-const BACKSPACE = "⌫";
+const BACKSPACE = "backspace";
 
 const VALID_LETTERS = [...QWERTY, ...ASDF, ...ZXCV];
 
@@ -99,7 +99,7 @@ function App() {
       return;
     } else if (guess.length === 5 && VALID_LETTERS.includes(letter)) {
       return;
-    } else if (letter === "⌫") {
+    } else if (letter === "backspace") {
       setGuess((prev) => prev.slice(0, -1));
     } else {
       setGuess((prev) => prev.concat(letter));
@@ -113,12 +113,27 @@ function App() {
           <div
             key={letter}
             onClick={() => simulateKeypress(letter)}
-            className={`flex bg-gray-200 px-2 py-4 items-center justify-center text-xs md:text-lg font-bold rounded uppercase hover:cursor-pointer${
-              letter === "⌫" ? " text-xl" : ""
-            }`}
+            className="flex bg-gray-200 px-2 py-4 items-center justify-center text-xs md:text-lg font-bold rounded uppercase hover:cursor-pointer"
             style={{ minWidth: "30px" }}
           >
-            {letter}
+            {letter === "backspace" ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M3 12l6.414 6.414a2 2 0 001.414.586H19a2 2 0 002-2V7a2 2 0 00-2-2h-8.172a2 2 0 00-1.414.586L3 12z"
+                />
+              </svg>
+            ) : (
+              letter
+            )}
           </div>
         ))}
       </div>
