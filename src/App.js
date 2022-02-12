8713bad15d92437eed565023027bc33ba02529e1
JP Sio <hello@jpsio.me>
2022-02-11 23:57:33 -0800

    Try confetti

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect, useRef } from "react";
 import ReactCardFlip from "react-card-flip";
 import useKeypress from "react-use-keypress";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 import "./App.css";
 
@@ -41,6 +43,7 @@ function App() {
   const [guess, setGuess] = useState([]);
   const [currentGuess, setCurrentGuess] = useState(0);
   const timeoutRef = useRef(null);
+  const { width, height } = useWindowSize();
 
   useKeypress("Enter", (event) => {
     if (guess.length === 5) {
@@ -129,62 +132,65 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col justify-between">
-      <div>
-        <h1 className="title border-b">Wordle</h1>
-        <div
-          className="flex flex-wrap space-between align-center justify-between mx-auto mt-12"
-          style={{ width: "330px", height: "410px" }}
-        >
-          {Array(30)
-            .fill("")
-            .map((space, index) => (
-              <ReactCardFlip key={index} isFlipped={flipMap[index]} flipDirection="vertical">
-                <div className="square flex items-center align-center justify-center">{guess[index]}</div>
+    <>
+      {flipMap["29"] && <Confetti width={width} height={height} />}
+      <div className="flex flex-col justify-between">
+        <div>
+          <h1 className="title border-b">Wordle</h1>
+          <div
+            className="flex flex-wrap space-between align-center justify-between mx-auto mt-12"
+            style={{ width: "330px", height: "410px" }}
+          >
+            {Array(30)
+              .fill("")
+              .map((space, index) => (
+                <ReactCardFlip key={index} isFlipped={flipMap[index]} flipDirection="vertical">
+                  <div className="square flex items-center align-center justify-center">{guess[index]}</div>
 
-                <div className="square flex items-center align-center justify-center">
-                  <img src={require(`./squares/${index}.jpg`)} alt="" />
-                </div>
-              </ReactCardFlip>
+                  <div className="square flex items-center align-center justify-center">
+                    <img src={require(`./squares/${index}.jpg`)} alt="" />
+                  </div>
+                </ReactCardFlip>
+              ))}
+          </div>
+        </div>
+        <div className="my-3 space-y-3 flex flex-col items-center">
+          <div className="flex space-x-2">
+            {KEYS_ROW_1.map((letter) => (
+              <div
+                key={letter}
+                onClick={() => simulateKeypress(letter)}
+                className="align-center bg-gray-200 flex font-bold items-center justify-center key rounded uppercase hover:cursor-pointer"
+              >
+                {letter}
+              </div>
             ))}
+          </div>
+          <div className="flex mx-auto space-x-2">
+            {KEYS_ROW_2.map((letter) => (
+              <div
+                key={letter}
+                onClick={() => simulateKeypress(letter)}
+                className="align-center bg-gray-200 flex font-bold items-center justify-center key rounded uppercase hover:cursor-pointer"
+              >
+                {letter}
+              </div>
+            ))}
+          </div>
+          <div className="flex mx-auto space-x-2">
+            {KEYS_ROW_3.map((letter) => (
+              <div
+                key={letter}
+                onClick={() => simulateKeypress(letter)}
+                className="align-center bg-gray-200 flex font-bold items-center justify-center key rounded uppercase hover:cursor-pointer px-3"
+              >
+                {letter}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="my-3 space-y-3 flex flex-col items-center">
-        <div className="flex space-x-2">
-          {KEYS_ROW_1.map((letter) => (
-            <div
-              key={letter}
-              onClick={() => simulateKeypress(letter)}
-              className="align-center bg-gray-200 flex font-bold items-center justify-center key rounded uppercase hover:cursor-pointer"
-            >
-              {letter}
-            </div>
-          ))}
-        </div>
-        <div className="flex mx-auto space-x-2">
-          {KEYS_ROW_2.map((letter) => (
-            <div
-              key={letter}
-              onClick={() => simulateKeypress(letter)}
-              className="align-center bg-gray-200 flex font-bold items-center justify-center key rounded uppercase hover:cursor-pointer"
-            >
-              {letter}
-            </div>
-          ))}
-        </div>
-        <div className="flex mx-auto space-x-2">
-          {KEYS_ROW_3.map((letter) => (
-            <div
-              key={letter}
-              onClick={() => simulateKeypress(letter)}
-              className="align-center bg-gray-200 flex font-bold items-center justify-center key rounded uppercase hover:cursor-pointer px-3"
-            >
-              {letter}
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
