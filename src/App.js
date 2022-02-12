757faa315a7e03ed1643d9cd5c07337cf0dcb47d
JP Sio <hello@jpsio.me>
2022-02-11 23:19:22 -0800

    Try nonfunctioning keyboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import useKeypress from "react-use-keypress";
 
 import "./App.css";
 
+const KEYS_ROW_1 = ["q", "w", "e", "t", "y", "u", "i", "o", "p"];
+const KEYS_ROW_2 = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
+const KEYS_ROW_3 = [".", "z", "x", "c", "v", "b", "n", "m", ","];
+
 function App() {
   const [flipMap, setFlipMap] = useState({});
   const [guess, setGuess] = useState([]);
@@ -72,7 +76,6 @@ function App() {
     }
 
     timeoutRef.current = setTimeout(() => {
-      console.log("here", currentGuess);
       setCurrentGuess((prev) => prev + 1);
       setFlipMap((prev) => ({
         ...prev,
@@ -82,22 +85,56 @@ function App() {
   }, [currentGuess]);
 
   return (
-    <>
-      <h1 className="title border-b">Wordle</h1>
-      <div className="container flex flex-wrap space-between mx-auto mt-16">
-        {Array(30)
-          .fill("")
-          .map((space, index) => (
-            <ReactCardFlip key={index} isFlipped={flipMap[index]} flipDirection="vertical">
-              <div className="square flex items-center align-center justify-center">{guess[index]}</div>
+    <div className="flex flex-col justify-between">
+      <div>
+        <h1 className="title border-b">Wordle</h1>
+        <div className="container flex flex-wrap space-between mx-auto mt-12">
+          {Array(30)
+            .fill("")
+            .map((space, index) => (
+              <ReactCardFlip key={index} isFlipped={flipMap[index]} flipDirection="vertical">
+                <div className="square flex items-center align-center justify-center">{guess[index]}</div>
 
-              <div className="square flex items-center align-center justify-center">
-                <img src={require(`./squares/${index}.jpg`)} alt="" />
-              </div>
-            </ReactCardFlip>
-          ))}
+                <div className="square flex items-center align-center justify-center">
+                  <img src={require(`./squares/${index}.jpg`)} alt="" />
+                </div>
+              </ReactCardFlip>
+            ))}
+        </div>
       </div>
-    </>
+      <div className="my-3 space-y-3 flex flex-col items-center">
+        <div className="flex space-x-2">
+          {KEYS_ROW_1.map((letter) => (
+            <div
+              key={letter}
+              className="align-center bg-gray-200 flex font-bold items-center justify-center key rounded uppercase hover:cursor-pointer"
+            >
+              {letter}
+            </div>
+          ))}
+        </div>
+        <div className="flex mx-auto space-x-2">
+          {KEYS_ROW_2.map((letter) => (
+            <div
+              key={letter}
+              className="align-center bg-gray-200 flex font-bold items-center justify-center key rounded uppercase hover:cursor-pointer"
+            >
+              {letter}
+            </div>
+          ))}
+        </div>
+        <div className="flex mx-auto space-x-2">
+          {KEYS_ROW_3.map((letter) => (
+            <div
+              key={letter}
+              className="align-center bg-gray-200 flex font-bold items-center justify-center key rounded uppercase hover:cursor-pointer"
+            >
+              {letter}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
